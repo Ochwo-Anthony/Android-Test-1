6e3187f8b4518e7cd33438bf00d86fbcd629ef13
ST10395938 <130601150+Ochwo-Anthony@users.noreply.github.com>
2026-03-23 09:23:07 +0200

    Initial Commit
    Greeting App
    - 8 languages
    - 1 screen

---
+++ app/src/main/java/com/example/greetingapp/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.greetingapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // TextView
+        val textView = findViewById<TextView>(R.id.textView)
+
+        // Buttons
+        val btnEnglish = findViewById<Button>(R.id.btnEnglish)
+        val btnZulu = findViewById<Button>(R.id.btnZulu)
+        val btnAfrikaans = findViewById<Button>(R.id.btnAfrikaans)
+        val btnFrench = findViewById<Button>(R.id.btnFrench)
+        val btnSpanish = findViewById<Button>(R.id.btnSpanish)
+        val btnGerman = findViewById<Button>(R.id.btnGerman)
+        val btnChinese = findViewById<Button>(R.id.btnChinese)
+        val btnArabic = findViewById<Button>(R.id.btnArabic)
+
+        // Click Listeners
+        btnEnglish.setOnClickListener {
+            textView.text = "Hello"
+        }
+
+        btnZulu.setOnClickListener {
+            textView.text = "Sawubona"
+        }
+
+        btnAfrikaans.setOnClickListener {
+            textView.text = "Hallo"
+        }
+
+        btnFrench.setOnClickListener {
+            textView.text = "Bonjour"
+        }
+
+        btnSpanish.setOnClickListener {
+            textView.text = "Hola"
+        }
+
+        btnGerman.setOnClickListener {
+            textView.text = "Hallo"
+        }
+
+        btnChinese.setOnClickListener {
+            textView.text = "Ni Hao"
+        }
+
+        btnArabic.setOnClickListener {
+            textView.text = "Marhaba"
+        }
+
+        }
+    }
